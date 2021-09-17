023021c7ed9f6cddc21796c1ef5df3635d9653b0
Fabio Santos <Fabio.Santos@tns-ilres.com>
2021-09-17 12:40:53 +0200

    update select coins to pay drink

--- drink_dispenser/Program.cs
+++ drink_dispenser/Program.cs
@@ -13,11 +13,6 @@ namespace drink_distributor
             coinValue = Value;
             coinQuantity = Quantity;
         }
-
-        /*public void DisplayValues
-        {
-
-        }*/
     }
 
 
@@ -30,49 +25,37 @@ namespace drink_distributor
         static float cocaPrice = 1.50f;
         static float fantaPrice = 1.60f;
         static float spritePrice = 1.25f;
-        static string coinsValue = "0";
         static float coinsValueF = 0.0f;
 
-        float coin1cent = 0.01f;
-        int coin1centQuantity = 27;
-
-        float coin2cents = 0.02f;
-        int coin2centsQuantity = 32;
-
-        float coin5cents = 0.05f;
-        int coin5centsQuantity = 26;
-
-        float coin10cents = 0.10f;
-        int coin10centsQuantity = 60;
-
-        float coin20cents = 0.20f;
-        int coin20centsQuantity = 40;
-
-        float coin50cents = 0.50f;
-        int coin50centsQuantity = 20;
-
-        int coin1eur = 1;
-        int coin1eurQuantity = 25;
-
-        int coin2eur = 2;
-        int coin2eurQuantity = 30;
-
-
+ 
         static string selectedDrink;
+
+        static CoinsValueQuantity coin1C = new CoinsValueQuantity(0.01f, 13);
+        static CoinsValueQuantity coin2C = new CoinsValueQuantity(0.02f, 45);
+        static CoinsValueQuantity coin5C = new CoinsValueQuantity(0.05f, 31);
+        static CoinsValueQuantity coin10C = new CoinsValueQuantity(0.10f, 22);
+        static CoinsValueQuantity coin20C = new CoinsValueQuantity(0.20f, 33);
+        static CoinsValueQuantity coin50C = new CoinsValueQuantity(0.50f, 0);
+        static CoinsValueQuantity coin1Eur = new CoinsValueQuantity(1, 74);
+        static CoinsValueQuantity coin2Eur = new CoinsValueQuantity(2, 44);
 
 
 
         static void SelectDrink()
         {
+            Console.WriteLine("_____________________________________________");
+            Console.WriteLine("");
             Console.WriteLine("Hello");
+            Console.WriteLine();
             Console.WriteLine("Please select a drink : ");
+            
             Console.WriteLine("1 : " + coca);
             Console.WriteLine("2 : " + fanta);
             Console.WriteLine("3 : " + sprite);
-
+            Console.WriteLine();
             selectedDrink = Console.ReadLine();
             int selectedDrink_num = int.Parse(selectedDrink);
-
+            
 
             switch (selectedDrink_num)
             {
@@ -100,6 +83,57 @@ namespace drink_distributor
             }
         }
 
+
+        static float SelectedCoin()
+        {
+            Console.WriteLine("Select coin for paying");
+            Console.WriteLine("______________________________________");
+            Console.WriteLine("[1]" + coin1C.coinValue + " cent");
+            Console.WriteLine("[2]" + coin2C.coinValue + " cents");
+            Console.WriteLine("[3]" + coin5C.coinValue + " cents");
+            Console.WriteLine("[4]" + coin10C.coinValue + " cents");
+            Console.WriteLine("[5]" + coin20C.coinValue + " cents");
+            Console.WriteLine("[6]" + coin50C.coinValue + " cents");
+            Console.WriteLine("[7]" + coin1Eur.coinValue + " euro");
+            Console.WriteLine("[8]" + coin2Eur.coinValue + " euro");
+
+
+            string selectedcoin = Console.ReadLine();
+            int selectedcoin_num = int.Parse(selectedcoin);
+            float coinSelected = 0.0f;
+
+            switch (selectedcoin_num)
+            {
+                case 1:
+                    coinSelected = coin1C.coinValue;
+                    break;
+                case 2:
+                    coinSelected = coin2C.coinValue;
+                    break;
+                case 3:
+                    coinSelected = coin5C.coinValue;
+                    break;
+                case 4:
+                    coinSelected = coin10C.coinValue;
+                    break;
+                case 5:
+                    coinSelected = coin20C.coinValue;
+                    break;
+                case 6:
+                    coinSelected = coin50C.coinValue;
+                    break;
+                case 7:
+                    coinSelected = coin1Eur.coinValue;
+                    break;
+                case 8:
+                    coinSelected = coin2Eur.coinValue;
+                    break;
+            }
+
+            return coinSelected;
+
+        }
+
         static void PayDrink(int drinkSelection)
         {
 
@@ -110,22 +144,22 @@ namespace drink_distributor
                     case 1:
                         Console.WriteLine();
                         Console.WriteLine("Please pay " + cocaPrice + " € for " + coca);
-                        coinsValue = Console.ReadLine();
-                        coinsValueF = float.Parse(coinsValue);
+                        Console.WriteLine();
+                        coinsValueF = SelectedCoin();
                         CalculateDrinkPrice(coinsValueF, cocaPrice);
                         break;
                     case 2:
                         Console.WriteLine();
                         Console.WriteLine("Please pay " + fantaPrice + " € for " + fanta);
-                        coinsValue = Console.ReadLine();
-                        coinsValueF = float.Parse(coinsValue);
+                        Console.WriteLine();
+                        coinsValueF = SelectedCoin();
                         CalculateDrinkPrice(coinsValueF, fantaPrice);
                         break;
                     case 3:
                         Console.WriteLine();
                         Console.WriteLine("Please pay " + spritePrice + " € for " + sprite);
-                        coinsValue = Console.ReadLine();
-                        coinsValueF = float.Parse(coinsValue);
+                        Console.WriteLine();
+                        coinsValueF = SelectedCoin();
                         CalculateDrinkPrice(coinsValueF, spritePrice);
                         break;
                 }
@@ -146,9 +180,7 @@ namespace drink_distributor
                 moneyDifference = RoundNumber(moneyDifference);
                 Console.WriteLine("there is still " + moneyDifference + "€ missing");
 
-                string missingMoneyValue = Console.ReadLine();
-                float missingMoneyValueF = float.Parse(missingMoneyValue);
-                missingMoneyValueF = RoundNumber(missingMoneyValueF);
+                float missingMoneyValueF = SelectedCoin();
 
                 moneyDifference = moneyDifference - missingMoneyValueF;
                 moneyDifference = RoundNumber(moneyDifference);
@@ -157,24 +189,36 @@ namespace drink_distributor
                 {
                     Console.WriteLine();
                     Console.WriteLine("there is still " + moneyDifference + "€ missing");
-                    missingMoneyValue = Console.ReadLine();
-                    missingMoneyValueF = float.Parse(missingMoneyValue);
+                    missingMoneyValueF = SelectedCoin();
+                   
                     moneyDifference = moneyDifference - missingMoneyValueF;
                     moneyDifference = RoundNumber(moneyDifference);
                 }
+                ReturnMoney(moneyDifference);
             }
             else
             {
                 moneyDifference = drinkPrice - coins;
                 ReturnMoney(moneyDifference);
-
             }
             Console.WriteLine();
+
+            moneyDifference = ConvertToPositivNumber(moneyDifference);
             moneyDifference = RoundNumber(moneyDifference);
             Console.WriteLine("Here is your change " + moneyDifference + "€ ");
+            Console.WriteLine();
             Console.WriteLine("And drink number " + selectedDrink);
 
             SelectDrink();
+        }
+
+
+        // Convert negativ number into positiv
+        static float ConvertToPositivNumber(float number)
+        {
+            number = number * -1;
+
+            return number;
         }
 
         // Round Number to 2 decimal places
@@ -188,14 +232,7 @@ namespace drink_distributor
         static void ReturnMoney(float moneyGiveBack)
         {
             moneyGiveBack = RoundNumber(moneyGiveBack);
-            CoinsValueQuantity coin1C = new CoinsValueQuantity(0.01f, 13);
-            CoinsValueQuantity coin2C = new CoinsValueQuantity(0.02f, 45);
-            CoinsValueQuantity coin5C = new CoinsValueQuantity(0.05f, 31);
-            CoinsValueQuantity coin10C = new CoinsValueQuantity(0.10f, 22);
-            CoinsValueQuantity coin20C = new CoinsValueQuantity(0.20f, 33);
-            CoinsValueQuantity coin50C = new CoinsValueQuantity(0.50f, 0);
-            CoinsValueQuantity coin1Eur = new CoinsValueQuantity(1, 74);
-            CoinsValueQuantity coin2Eur = new CoinsValueQuantity(2, 44);
+            moneyGiveBack = ConvertToPositivNumber(moneyGiveBack);
 
             Console.WriteLine("Coins before Quantity : " + coin1C.coinQuantity + " / value : " + coin1C.coinValue);
             Console.WriteLine("Coins before Quantity : " + coin2C.coinQuantity + " / value : " + coin2C.coinValue);
@@ -206,11 +243,7 @@ namespace drink_distributor
             Console.WriteLine("Coins before Quantity : " + coin1Eur.coinQuantity + " / value : " + coin1Eur.coinValue);
             Console.WriteLine("Coins before Quantity : " + coin2Eur.coinQuantity + " / value : " + coin2Eur.coinValue);
 
-            if(moneyGiveBack < 0)
-            {
-                moneyGiveBack = moneyGiveBack * -1;
-            }
-            Console.WriteLine("Monnaie rendu : ("+ coinsValueF + ")");
+            Console.WriteLine("Monnaie rendu de : (" + coinsValueF + ")");
 
             while (moneyGiveBack != 0.0f)
             {
@@ -228,42 +261,42 @@ namespace drink_distributor
                     coin1Eur.coinQuantity = coin1Eur.coinQuantity - 1;
                     Console.WriteLine("\t - Piece : 1 euro");
                 }
-                else if((coin50C.coinValue <= moneyGiveBack) && (coin50C.coinQuantity >= 1))
+                else if ((coin50C.coinValue <= moneyGiveBack) && (coin50C.coinQuantity >= 1))
                 {
                     moneyGiveBack = moneyGiveBack - coin50C.coinValue;
                     moneyGiveBack = RoundNumber(moneyGiveBack);
                     coin50C.coinQuantity = coin50C.coinQuantity - 1;
                     Console.WriteLine("\t - Piece : 50 cents");
                 }
-                else if((coin20C.coinValue <= moneyGiveBack) && (coin20C.coinQuantity >= 1))
+                else if ((coin20C.coinValue <= moneyGiveBack) && (coin20C.coinQuantity >= 1))
                 {
                     moneyGiveBack = moneyGiveBack - coin20C.coinValue;
                     moneyGiveBack = RoundNumber(moneyGiveBack);
                     coin20C.coinQuantity = coin20C.coinQuantity - 1;
                     Console.WriteLine("\t - Piece : 20 cents");
                 }
-                else if((coin10C.coinValue <= moneyGiveBack) && (coin10C.coinQuantity >= 1))
+                else if ((coin10C.coinValue <= moneyGiveBack) && (coin10C.coinQuantity >= 1))
                 {
                     moneyGiveBack = moneyGiveBack - coin10C.coinValue;
                     moneyGiveBack = RoundNumber(moneyGiveBack);
                     coin10C.coinQuantity = coin10C.coinQuantity - 1;
                     Console.WriteLine("\t - Piece : 10 cents");
                 }
-                else if((coin5C.coinValue <= moneyGiveBack) && (coin5C.coinQuantity >= 1))
+                else if ((coin5C.coinValue <= moneyGiveBack) && (coin5C.coinQuantity >= 1))
                 {
                     moneyGiveBack = moneyGiveBack - coin5C.coinValue;
                     moneyGiveBack = RoundNumber(moneyGiveBack);
                     coin5C.coinQuantity = coin5C.coinQuantity - 1;
                     Console.WriteLine("\t - Piece : 5 cents");
                 }
-                else if((coin2C.coinValue <= moneyGiveBack) && (coin2C.coinQuantity >= 1))
+                else if ((coin2C.coinValue <= moneyGiveBack) && (coin2C.coinQuantity >= 1))
                 {
                     moneyGiveBack = moneyGiveBack - coin2C.coinValue;
                     moneyGiveBack = RoundNumber(moneyGiveBack);
                     coin2C.coinQuantity = coin2C.coinQuantity - 1;
                     Console.WriteLine("\t - Piece : 2 cents");
                 }
-                else if((coin1C.coinValue <= moneyGiveBack) && (coin1C.coinQuantity >= 1))
+                else if ((coin1C.coinValue <= moneyGiveBack) && (coin1C.coinQuantity >= 1))
                 {
                     moneyGiveBack = moneyGiveBack - coin1C.coinValue;
                     moneyGiveBack = RoundNumber(moneyGiveBack);
@@ -295,10 +328,7 @@ namespace drink_distributor
             // Convert special symbols to show on console ex : €
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-
-
             SelectDrink();
-
 
         }
     }
